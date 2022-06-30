6489bf66784e069543f49b252d51cfac1e70b8ee
infomagnum159 <infomagnum159@gmail.com>
2022-06-30 16:37:16 +0600

    Added URL

---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const BASE_URL = 'http://146.185.154.90:8080/restcountries/rest/v2/';
+export const COUNTRY_URL = 'all?fields=name;alpha3Code';
+export const BORDER_URL = 'alpha/';
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import axios from "axios";
 import App from './App';
+import {BASE_URL} from "./config";
+import './index.css';
 
+
+axios.defaults.baseURL = BASE_URL;
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
+
